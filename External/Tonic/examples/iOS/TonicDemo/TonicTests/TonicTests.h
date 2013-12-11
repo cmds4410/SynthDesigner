7baaa2bc1f35abd3550c4c0b49756fcf22fe01c2
Connor Smith <cmds4410@gmail.com>
2013-12-10 23:27:14 -0500

    Builds with Tonic and The Amazing Audio Engine

---
+++ External/Tonic/examples/iOS/TonicDemo/TonicTests/TonicTests.h
@@ -0,0 +1,13 @@
+//
+//  TonicTests.h
+//  TonicTests
+//
+//  Created by Nick D on 4/19/13.
+//  Copyright (c) 2013 Morgan Packard. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TonicTests : SenTestCase
+
+@end
